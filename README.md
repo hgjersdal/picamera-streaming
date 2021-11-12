5647776eb7a393c26924ae58efa46123d137cd5c
haavagj <haavagj@uio.no>
2021-11-12 15:19:34 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+# picamera-test
+
+- `p` to take an image. Saved as img?.jpg, where ? starts at 0 and increments by one per press
+- `z` to toggle between 100% crop and full screen (assuming 2560x1440 screen)
+- `q` quit
---
+++ picamera-test.py
@@ -0,0 +1,45 @@
+import picamera
+import time
+import threading 
+import keyboard
+
+keep_looping = True 
+camera = picamera.PiCamera()
+
+def check_input_thread():
+    global keep_looping
+    global camera
+    counter = 0
+    resolution = camera.resolution
+    zoom = camera.zoom
+    while(True):
+        key = keyboard.read_key()
+        if(key == 'p'):
+            camera.resolution=(4056, 3040)
+            camera.capture('img' + str(counter) + '.jpg')
+            counter += 1
+            camera.resolution=resolution
+        elif(key == 'z'):
+            if resolution == camera.resolution:
+                camera.resolution=(4056, 3040)
+                camera.zoom = (0.33, 0.33, 0.33, 0.33)
+            else:
+                camera.resolution=resolution
+                camera.zoom = zoom
+        elif(key == 'q'):
+            keep_looping = False
+            break
+
+def preview():
+    camera.start_preview()
+    print(camera.resolution)
+    print(camera.zoom)
+    threading.Thread(target=check_input_thread, args=(), name='check_input', daemon=True).start()
+    while(keep_looping):
+        pass
+
+
+    print(camera.resolution)
+    camera.stop_preview()
+
+preview()
